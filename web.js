96f84e166f391d851ae5190303269fa76130ab2f
Rutvik Sheth <rutvik.sheth@gmail.com>
2013-07-09 22:19:48 +0000

    added web.js with changes to read index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+fs.readFileSync('index.html',function(err,data){
+ if(err) throw err;
+ });
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(console.log(data.tostring('utf-8')));
 });
 
 var port = process.env.PORT || 5000;
