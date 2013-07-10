19a9c8899e03861ce5b27ff80fd555f7a365fc9e
Rutvik Sheth <rutvik.sheth@gmail.com>
2013-07-10 04:38:09 +0000

    edit 5

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
-var express = require('express');
+>0;271;0cvar express = require('express');
 
 var app = express.createServer(express.logger());
 
 var fs=require('fs');
 var data=fs.readFileSync('index.html');
 
-var buf=new Buffer(data,'utf-8')
+var buf=new Buffer(data,'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(console.log(buf.toString('utf-8')));
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
